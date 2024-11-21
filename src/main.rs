35e18af0a083110cc7b8d0fd9f79e5ed815af7f4
zimrustpro <zimrustpro@gmail.com>
2024-11-21 16:40:05 +0200

    |_| in a closure

--- src/main.rs
+++ src/main.rs
@@ -66,4 +66,9 @@ fn main() {
         }
     }
     println!();
+
+    let my_vec = vec![8, 9, 10];
+    my_vec
+        .iter()
+        .for_each(|_| println!("We didn't use the variables at all"));
 }
